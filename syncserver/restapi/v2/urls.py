51f0c457642ee101096b2ea007ad40061e772c33
William Swanson <swansontec@gmail.com>
2016-10-07 10:59:28 -0700

    Fix empty-repo edge cases in the sync API
    These happen when the repo is first created,
    making it impossible to fetch or add things.
    
    The commit ID is now an optional part of the URI,
    instead of being a part of the request body.
    This is necessary because GET requests don't have bodies.

--- syncserver/restapi/v2/urls.py
+++ syncserver/restapi/v2/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import patterns, url
 import views as v
 
 urlpatterns = patterns('',
-     url(r'^store/(?P<storeId>.+)?$', v.RepoStore.as_view(), name="repo-store")
+     url(r'^store/(?P<storeId>[0-9a-fA-F]+)/?$', v.RepoStore.as_view(), name="repo-store"),
+     url(r'^store/(?P<storeId>[0-9a-fA-F]+)/(?P<start_hash>[0-9a-fA-F]+)/?$', v.RepoStore.as_view(), name="repo-commit"),
 )
 
 
--- syncserver/restapi/v2/views.py
+++ syncserver/restapi/v2/views.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from rest_framework import status
-from rest_framework.response import Response 
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from subprocess import check_call, check_output, CalledProcessError
 from django.conf import settings
@@ -11,40 +11,55 @@ from restapi.utils import create_repo
 
 TMP_DIR = '/tmp'
 
-def git_rev(cwd):
-    return check_output(['git', 'rev-parse', 'HEAD'], cwd=cwd).strip()
+def create_path(storeId):
+    prefix = storeId[0:2]
+    return "{0}/{1}/{2}".format(settings.REPO_PATH, prefix, storeId)
+
+def git_hash_found(path, git_hash):
+    try:
+        check_output(['git', 'merge-base', 'HEAD', git_hash], cwd=path)
+        return True
+    except:
+        return False
+
+def git_head(path):
+    try:
+        return check_output(['git', 'rev-parse', '--verify', 'HEAD'], cwd=path).strip()
+    except:
+        return None
 
 def git_changed_files(path, rev=None):
     if rev:
-        output = check_output(['git', 'log', '{0}..HEAD'.format(rev), '--name-only', '--pretty=format:'], cwd=path)
+        output = check_output(['git', 'log', '--name-only', '--pretty=format:', '{0}..HEAD'.format(rev)], cwd=path)
     else:
         output = check_output(['git', 'log', '--name-only', '--pretty=format:'], cwd=path)
     # strip output, split by new lines and remove any empty lines
     return filter(lambda x : x, set(output.strip().split("\n")))
 
+def git_show(path, filepath):
+    return check_output(['git', 'show', 'HEAD:{0}'.format(filepath)], cwd=path).strip()
+
 def git_file_lists(path, rev=None):
-    changed_files=git_changed_files(path, rev=rev)
+    changed_files = git_changed_files(path, rev=rev)
     file_contents = {}
     print changed_files
     for f in changed_files:
-        file_contents[f] = git_show(f, path)
+        file_contents[f] = git_show(path, f)
     return file_contents
 
-def git_show(filepath, path):
-    return check_output(['git', 'show', 'HEAD:{0}'.format(filepath)], cwd=path).strip()
-
-def create_path(storeId):
-    prefix = storeId[0:2]
-    return "{0}/{1}/{2}".format(settings.REPO_PATH, prefix, storeId)
-
 def git_change_dict(path, rev=None):
-    current_hash=git_rev(path)
-    file_contents=git_file_lists(path, rev=rev)
-    data={
-        "hash": current_hash,
-        "changes": [{'key': k, 'value': v } for k,v in file_contents.iteritems()]
-    }
-    return data
+    current_hash = git_head(path)
+    if not current_hash:
+        # empty repo
+        return {
+            "changes": []
+        }
+    else:
+        file_contents = git_file_lists(path, rev=rev)
+        return {
+            "hash": current_hash,
+            "changes": [{'key': k, 'value': v} for k,v in file_contents.iteritems()]
+        }
 
 def gen_id():
     return ''.join([random.choice(string.digits + string.letters) for i in range(0, 30)])
@@ -53,8 +68,7 @@ def git_update(path, storeId, changes, start_hash=None):
     check_output(['git', 'config', '--global', 'user.email', 'api@airbitz.co'])
     check_output(['git', 'config', '--global', 'user.name', 'Airbitz API'])
 
-    new_repo_name="{0}{1}".format(storeId, gen_id())
-    working_tree='{0}/{1}'.format(TMP_DIR, new_repo_name)
+    working_tree='{0}/{1}{2}'.format(TMP_DIR, storeId, gen_id())
     print working_tree
     os.makedirs(working_tree)
     for change in changes:
@@ -72,51 +86,39 @@ def git_update(path, storeId, changes, start_hash=None):
         print 'Working Tree', working_tree
         print e
 
-def git_hash_found(cwd, git_hash):
-    try:
-        check_output(['git', 'merge-base', 'HEAD', git_hash], cwd=cwd)
-        return True
-    except:
-        return False
-
 class RepoStore(APIView):
     # Fetch latest
-    def get(self, request, storeId):
-        data = json.loads(request.body)
+    def get(self, request, storeId, start_hash=None):
         path = create_path(storeId)
         if not os.path.isdir(path):
             return Response(status=status.HTTP_404_NOT_FOUND)
-
-        start_hash=data.get("hash", None)
         if start_hash and not git_hash_found(path, start_hash):
             return Response(status=status.HTTP_404_NOT_FOUND, data={'msg': 'Hash not found'})
 
         try:
-            payload=git_change_dict(path, rev=start_hash)
+            payload = git_change_dict(path, rev=start_hash)
             return Response(status=status.HTTP_200_OK, data=payload)
         except Exception as e:
             print e
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     # Update repository
-    def post(self, request, storeId):
-        data = json.loads(request.body)
+    def post(self, request, storeId, start_hash=None):
         path = create_path(storeId)
         if not os.path.isdir(path):
             return Response(status=status.HTTP_404_NOT_FOUND)
-
-        start_hash=data.get("hash", None)
-        changes=data.get("changes", [])
         if start_hash and not git_hash_found(path, start_hash):
-            return Response(status=status.HTTP_404_NOT_FOUND, data=json.dumps({'msg': 'Hash not found'}))
+            return Response(status=status.HTTP_404_NOT_FOUND, data={'msg': 'Hash not found'})
+
         try:
-            git_update(path, storeId, changes, start_hash=start_hash)
-            payload=git_change_dict(path, rev=start_hash)
+            changes = json.loads(request.body).get("changes", [])
+            if len(changes):
+                git_update(path, storeId, changes, start_hash=start_hash)
+            payload = git_change_dict(path, rev=start_hash)
             return Response(status=status.HTTP_200_OK, data=payload)
         except Exception as e:
             print e
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
 
     # New repository
     def put(self, request, storeId):
