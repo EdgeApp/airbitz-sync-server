b9f63698be25715965b02ee460794e0436cb87dd
Tim Horton <tmhorton@gmail.com>
2016-11-07 08:06:04 -0800

    Upgraded to django 1.8
    Upgrade instructions:
    
        source .env/bin/activate
        pip install -r staging/requirements.txt
        pip uninstall south
        cd walletserver
        ./manage.py migrate --fake-initial

--- syncserver/restapi/v2/urls.py
+++ syncserver/restapi/v2/urls.py
@@ -1,11 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 import views as v
 
-urlpatterns = patterns('',
+urlpatterns = [
      url(r'^store/(?P<storeId>[0-9a-fA-F]+)/?$', v.RepoStore.as_view(), name="repo-store"),
      url(r'^store/(?P<storeId>[0-9a-fA-F]+)/(?P<start_hash>[0-9a-fA-F]+)/?$', v.RepoStore.as_view(), name="repo-commit"),
-)
-
-
+]
 
--- syncserver/syncserver/urls.py
+++ syncserver/syncserver/urls.py
@@ -1,11 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^api/v1/', include('restapi.v1.urls')),
-    (r'^api/v2/', include('restapi.v2.urls')),
-
+urlpatterns = [
+    url(r'^api/v1/', include('restapi.v1.urls')),
+    url(r'^api/v2/', include('restapi.v2.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
