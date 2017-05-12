ba7895d281191215a6fc05030fae3595a8d00d4d
Paul Puey <paul@airbitz.co>
2017-05-12 05:27:48 -0700

    Make repolist.js synchronous

--- bin/repolist.js
+++ bin/repolist.js
@@ -8,34 +8,26 @@ const std = ["pipe", "inherit", "inherit"]
 var gdate = new Date()
 var glog = gdate.toDateString() + ":" + gdate.toTimeString()
 
-console.log(glog + ' sync_repos.js starting')
+console.log(glog + ' repolist.js starting')
 const config = require('/etc/sync_repos.config.json')
 const rootDir = config.userDir + config.reposDir
 
 // Spin off the loop that creates the repolist for this machine
 function loopWriteRepoList() {
   console.log('ENTER loopWriteRepoList')
-  child_process.execFile('find', [ rootDir, '-maxdepth', '2', '-mindepth', '2'], {
-    stdio: std,
+  const stdout_string = child_process.execFileSync('find', [ rootDir, '-maxdepth', '2', '-mindepth', '2'], {
+    // stdio: std,
     cwd: config.userDir,
     killSignal: 'SIGKILL'
-  }, (error, stdout, stderr) => {
-    if (error) {
-      console.log (error)
-    }
-    console.log('loopWriteRepoList: writing repolist.txt')
-    fs.writeFile(config.repoListPath + 'repolist.txt', stdout, (err) => {
-      if (err) {
-        console.log (err)
-      } else {
-        console.log('loopWriteRepoList: writing repolist.txt SUCCESS')
-        setTimeout(() => {
-          loopWriteRepoList()
-        }, 300000)
-      }
-    });
-    console.log(stdout);
   })
+
+  console.log('loopWriteRepoList: writing repolist.txt')
+  fs.writeFileSync(config.repoListPath + 'repolist.txt', stdout_string)
+
+  console.log('loopWriteRepoList: writing repolist.txt SUCCESS')
+  setTimeout(() => {
+    loopWriteRepoList()
+  }, 300000)
 
 }
 
