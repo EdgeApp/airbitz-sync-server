0de7ddb97ec99178e21f7919280dcb945382e31d
Paul Puey <paul@airbitz.co>
2017-06-17 21:34:45 -0700

    Remove newlines from commit strings.

--- src/update_db_hashes.js
+++ src/update_db_hashes.js
@@ -28,7 +28,8 @@ async function mainLoop () {
       const repoName = localRepos[n]
       const localPath = _rootDir + '/' + repoName.substring(0, 2) + '/' + repoName
       // console.log(localPath)
-      const commit = childProcess.execFileSync('git', ['rev-parse', 'HEAD'], { timeout: 3000, cwd: localPath, killSignal: 'SIGKILL' })
+      let commit = childProcess.execFileSync('git', ['rev-parse', 'HEAD'], { encoding: 'utf8', timeout: 3000, cwd: localPath, killSignal: 'SIGKILL' })
+      commit = commit.replace(/(\r\n|\n|\r)/gm,"")
       // const commit = child_process.execFileSync('git', ['rev-parse', 'HEAD'], { timeout: 3000, stdio: std_noerr, cwd: localPath, killSignal: 'SIGKILL' })
       console.log('  [git rev-parse success] ' + commit)
       await writeDb(config.serverName, commit, repoName)
