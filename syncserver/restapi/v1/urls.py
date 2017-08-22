0b040b0b4652b822329afad53b02d3eec9142611
Paul Puey <paul@airbitz.co>
2017-08-22 14:25:44 -0700

    Redirect repo/create/ to JS version

--- syncserver/restapi/v1/urls.py
+++ syncserver/restapi/v1/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 import views as v
 
 urlpatterns = [
-     url(r'^repo/create/?$', v.RepoCreate.as_view(), name="repo-create"),
+     url(r'^repo/create/?$', v.RepoCreateJS.as_view(), name="repo-create"),
      url(r'^repo/createjs/?$', v.RepoCreateJS.as_view(), name="repo-create"),
      url(r'^repo/sync/(?P<storeId>[^/]+)/?$', v.RepoSync.as_view(), name="repo-sync")
 ]
