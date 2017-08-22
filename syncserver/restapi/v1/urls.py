0d98ad0b5e82d9dbb3dfae2305c55b992e1d96a2
Paul Puey <paul@airbitz.co>
2017-08-22 14:19:37 -0700

    Switch app server to always use JS version of create_repo

--- syncserver/restapi/v1/urls.py
+++ syncserver/restapi/v1/urls.py
@@ -4,7 +4,6 @@ import views as v
 
 urlpatterns = [
      url(r'^repo/create/?$', v.RepoCreate.as_view(), name="repo-create"),
-     url(r'^repo/createjs/?$', v.RepoCreateJS.as_view(), name="repo-create"),
      url(r'^repo/sync/(?P<storeId>[^/]+)/?$', v.RepoSync.as_view(), name="repo-sync")
 ]
 
