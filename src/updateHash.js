430ceac4655e8738b839d155efbc7cc7cf12050b
Paul Puey <paul@airbitz.co>
2017-11-08 03:10:10 -0800

    Convert updateHash cli tool

---
+++ src/updateHash.js
@@ -0,0 +1,16 @@
+/**
+ * Created by paul on 6/17/17.
+ * @flow
+ */
+const { updateHash } = require('./common/updateHashInner.js')
+
+async function main () {
+  if (process.argv.length < 5) {
+    console.log('Usage: updateHash.js [server] [repo] [hash]')
+    return -1
+  } else {
+    await updateHash(process.argv[2], process.argv[3], process.argv[4])
+  }
+}
+
+main()
