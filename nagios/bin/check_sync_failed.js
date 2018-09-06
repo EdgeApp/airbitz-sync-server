7e3e3f7acb9299e6ae7b42c32717c2a01f096321
Paul Puey <paul@edgesecure.co>
2018-09-06 13:14:41 -0700

    Remove nagios files and unused celeryd.conf
    nagios files moved to edge-devops

--- nagios/bin/check_sync_failed.js
+++
@@ -1,8 +0,0 @@
-const f = require('/home/bitz/failedRepos.json')
-if (f.length > 0) {
-  console.log('CRITICAL: Failed repos: ' + f.length.toString())
-  process.exit(2)
-} else {
-  console.log('SUCCESS')
-  process.exit(0)
-}
